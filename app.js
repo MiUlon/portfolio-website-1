3e33fb463f33e00e6e34ec2ae14f54ea806ecc68
MindeF MindeL <mindegi15@gmail.com>
2024-02-10 20:37:12 +0200

    fixing some small bugs

--- app.js
+++ app.js
@@ -4,24 +4,32 @@ const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelector('.main-content');
 
 function PageTransitions() {
+    // Button click active class
+
     for (let i = 0; i < sectBtn.length; i++) {
         sectBtn[i].addEventListener('click', function() {
             let currentBtn = document.querySelectorAll('.active-btn');
             currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-            this.className += (' active-btn');
+            this.className += ' active-btn';
         });
     };
+
+    // Sections Active class
 
     allSections.addEventListener('click', (e) => {
         const id = e.target.dataset.id;
         
-        if(id) {
+        if (id) {
+            // Remove selected from other buttons
+
             sectBtns.forEach((btn) => {
                 btn.classList.remove('active');
             });
         };
 
         e.target.classList.add('active');
+
+        // Hide other sections
 
         sections.forEach((section) => {
             section.classList.remove('active');
